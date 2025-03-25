c17d756fffb62dce2c853025258648f43a4c2ffc
William <ywwwtseng@gmail.com>
2025-03-26 01:00:11 +0800

    fix: clear grid overlapping area

--- src/libs/chart/Grid.ts
+++ src/libs/chart/Grid.ts
@@ -20,26 +20,41 @@ export class Grid {
     this.height = this.chart.innerBottom;
 
     this.chart.ctx.clearRect(this.left, this.top, this.width - 1, this.height - 1);
-    this.chart.ctx.beginPath();
-    this.chart.ctx.strokeStyle = "#2B3139";
-    this.chart.ctx.lineWidth = 1;
+
+    const canvas = document.createElement("canvas");
+    canvas.width = this.width;
+    canvas.height = this.height;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    ctx.beginPath();
+    ctx.strokeStyle = "#2B3139";
+    ctx.lineWidth = 1;
 
     axisBottom.ticks.forEach((tick) => {
       if (tick.x > axisRight.left) {
         return;
       }
-      this.chart.ctx.moveTo(tick.x, this.top);
-      this.chart.ctx.lineTo(tick.x, this.height);
+      ctx.moveTo(tick.x, this.top);
+      ctx.lineTo(tick.x, this.height);
     });
+
+    ctx.stroke();
 
     axisRight.ticks.forEach((tick) => {
       if (tick.y >= this.chart.innerBottom - 2) {
         return;
       }
 
-      this.chart.ctx.moveTo(this.left + this.chart.ctx.lineWidth, tick.y);
-      this.chart.ctx.lineTo(this.width + this.left, tick.y);
+      const yStart = tick.y - ctx.lineWidth / 2;
+      const lineHeight = ctx.lineWidth;
+      ctx.clearRect(this.left, yStart, this.width, lineHeight);
+
+      ctx.beginPath();
+      ctx.moveTo(this.left + ctx.lineWidth, tick.y);
+      ctx.lineTo(this.width + this.left, tick.y);
+      ctx.stroke();
     });
-    this.chart.ctx.stroke();
+
+    this.chart.ctx.drawImage(canvas, this.left, this.top, this.width, this.height);
   }
 }
